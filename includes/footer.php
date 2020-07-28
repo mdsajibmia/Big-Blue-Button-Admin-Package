f1f80e23b6a2e4d92384d32d1afdaa28bc1cb070
mdsajibmia <mdsajibmia64@gmail.com>
2020-07-28 16:46:32 +0600

    Signed-off-by: mdsajibmia <mdsajibmia64@gmail.com>

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>All rights reserved</b>
+      </div>
+      <strong>Copyright &copy; 2020 Brought To You By <a href="">Rafi-Touhid</a></strong>
+      </div>
+</footer>
+
+
+      
